ef28d4c04cc2f97546d48fe917e793af51369394
İlker Yıldırım <ilker.ceng@gmail.com>
2020-08-09 14:49:34 +0300

    error and loading temp components

---
+++ src/components/ErrorModal.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+//TODO: show error modal 
+export default function ErrorModal(props) {
+  return <div>Error...</div>;
+}
---
+++ src/components/Loading.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+//TODO: create better loading component
+export default function Loading(props) {
+  return <div>Loading...</div>;
+}
